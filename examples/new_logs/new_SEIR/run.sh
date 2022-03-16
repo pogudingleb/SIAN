a96e3c645427ffcfa13d061a29aef2240f622e30
Ilia <i.ilmer@icloud.com>
2022-03-16 13:17:39 -0400

    new logs

--- examples/new_logs/new_SEIR/run.sh
+++ examples/new_logs/new_SEIR/run.sh
@@ -1,5 +1,5 @@
 rm *.out && touch time.out
-for each in $(ls *.mpl); do
+for each in $(ls *_dc_*.mpl); do
     echo $each >>time.out
     (/usr/bin/time -f "elapsed,user+system,memory\n%e,=%U+%S,%M000\n" /local/maple2021/bin/maple $each) >output.out 2>>time.out
 done
